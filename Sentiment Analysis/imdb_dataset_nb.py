7beaf591945cc3ce16e36634f226bb9e686f3c60
Hassan <mhtariq366@gmail.com>
2025-08-13 22:40:52 +0500

    Update imdb_dataset_nb.py

--- Sentiment Analysis/imdb_dataset_nb.py
+++ Sentiment Analysis/imdb_dataset_nb.py
@@ -12,6 +12,7 @@ df = pd.read_csv('Sentiment Analysis/datasets/IMDB Dataset.csv')
 #   describe sentiment count
 print(df.groupby('sentiment').describe())
 
+#   set numerical values 1 and 0 to positive and negative labels respectively
 df['label'] = df['sentiment'].apply(lambda x: 1 if 'positive' in x else 0)
 
 #   split dataset into training and testing data. Train is 80% while test is 20%
@@ -23,9 +24,11 @@ train_x_count = vect.fit_transform(train_x)
 
 # print(train_x_count.toarray()[45:50])
 
+#   Call the Multinomial Naive bayes model
 nb_model_mn = MultinomialNB()
 nb_model_mn.fit(train_x_count, train_y)
 
 test_x_count = vect.transform(test_x)
 
+#   Print the accuracy of model
 print(f"Multinomial NB: {nb_model_mn.score(test_x_count, test_y)}")
