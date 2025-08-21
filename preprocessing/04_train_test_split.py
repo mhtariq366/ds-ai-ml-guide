7679fafb01e301251ca49e15eafc6cb3135ebec4
mhtariq366 <mhtariq366@gmail.com>
2025-08-21 22:36:05 +0500

    preprocessing - 04 train test split added

---
+++ preprocessing/04_train_test_split.py
@@ -0,0 +1,14 @@
+"""
+Train test split
+"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('Datasets/preprocessing01.csv')
+
+X_label = df.iloc[:, :-1].values
+y_label = df.iloc[:, -1:].values
+
+X_train, X_test, y_train, y_test = train_test_split(X_label, y_label, test_size=0.2)
+
+print(len(X_train))
