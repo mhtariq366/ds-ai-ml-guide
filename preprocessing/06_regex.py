44170ebe52280b1ce6de6646e074d306afecda2f
mhtariq366 <mhtariq366@gmail.com>
2025-08-24 20:57:43 +0500

    preprocessing - 06 regex added

---
+++ preprocessing/06_regex.py
@@ -0,0 +1,19 @@
+"""
+Regular expressions
+"""
+
+import re
+
+sentence = 'Hello everyone, this is python regular expressions practise.'
+
+"""
+search specific characters in a sequence, return character match and position (start, end)
+return only first instance of character
+"""
+print(re.search('on', sentence))
+
+
+
+
+
+
