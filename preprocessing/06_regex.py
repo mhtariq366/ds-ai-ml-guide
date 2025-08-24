1b222c3945af684680973ec84fa1925c0d8013da
mhtariq366 <mhtariq366@gmail.com>
2025-08-24 21:07:36 +0500

    preprocessing - 06 updated

--- preprocessing/06_regex.py
+++ preprocessing/06_regex.py
@@ -13,7 +13,25 @@ return only first instance of character
 print(re.search('on', sentence))
 
 
+"""
+finding how many times a sequence of character appear in the dataset.
+re.findall() function return list of all occurances of the character.
+"""
+print(re.findall('on', sentence))
 
 
+"""
+To split, dataset based on a specific character. Mostly useful in spliting data based on a period, comma etc.
+returns a list of all the data elements after split.
+"""
+print(re.split('on', sentence))
+
+
+"""
+to replace any characters with new sequence of characters, re.sub() function is used.
+first paramter is what you want to replace
+second paramter is what you want the characters to be replaced with
+"""
+print(re.sub('everyone', 'people', sentence))
 
 
