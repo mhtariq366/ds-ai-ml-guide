2e04a7b9d0706ba4b07e37405b302510750ba4f9
mhtariq366 <mhtariq366@gmail.com>
2025-08-26 20:07:03 +0500

    nlp folder and tasks added

---
+++ nlp/tasks.py
@@ -0,0 +1,17 @@
+"""
+1. Summarization
+2. Topic Modelling
+3. Speech Recognition
+4. Question Answer
+5. Sentiment Analysis / Opinion Mining
+6. Spell Checking
+7. Information Retrieval
+8. Text Classification
+9. Information Extraction
+10. Language / Machine Translation
+11. Conversation Bot/Agents
+12. NER
+13. Similarity/Association
+14. Part of Speech Tagging
+15. Next Word Prediction
+"""
