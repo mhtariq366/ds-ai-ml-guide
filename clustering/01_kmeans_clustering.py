ac9b8ee39dd7bbb609fd9a2acccf2469670c5cb4
mhtariq366 <mhtariq366@gmail.com>
2025-08-26 19:41:15 +0500

    k means comments

--- clustering/01_kmeans_clustering.py
+++ clustering/01_kmeans_clustering.py
@@ -2,12 +2,21 @@
 KMeans Clustering
 
 This comes under the unsupervised learning. In this we do not have labeled input data for different classes
-instead, we only have data and our goal is to distinctly separate data into different cluster based on 
+instead, we just have data and our goal is to distinctly separate this data into different cluster based on 
 high similarity among intra cluster elements and low similarity among inter clusters.
 
 For this, we need to supply how many clusters, whats the value of k?
 Elbow method is used to determine the best value of k.
-Calculate error for each number of k. 
+Calculate error for each number of k. And we can see which value of k is appropriate.
+
+When k is choosen. Assign centroids. Distance for each point will be calculated from the centroids.
+Simply explained:
+1. choose centroids
+2. draw straight line between them.
+3. draw straight perpendicular line to line in step 2.
+4. all points on one side of centroid belongs to that centroid. and other side points belong to other centroids.
+5. repeat steps 1-4 until elements are change clusters.
+6. Once elements stop changing clusters assigned. stop the algorithm.
 
 """
 import pandas as pd
