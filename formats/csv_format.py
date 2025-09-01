f399b8c5efbe10db9740b22a9232962093a1eb8a
mhtariq366 <mhtariq366@gmail.com>
2025-09-01 18:56:24 +0500

    formats - added csv format

---
+++ formats/csv_format.py
@@ -0,0 +1,18 @@
+"""
+csv files are comma separated values file used often times when dealing with data.
+
+They are represented using commas to separate values in each record or line.
+"""
+
+
+marks = '''
+
+roll_no, name, marks
+501, Jon, 98
+601, Bob, 56
+701, Akram, 83
+
+'''
+
+
+print(marks)
