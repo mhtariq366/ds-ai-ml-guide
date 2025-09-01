488a249b6e63db0a7ec522b6dbde843dc156759c
mhtariq366 <mhtariq366@gmail.com>
2025-09-01 19:26:43 +0500

    dip - read image updated

--- digital image processing/read_image.py
+++ digital image processing/read_image.py
@@ -1,5 +1,12 @@
 import cv2
 
+"""
+to read an image in opencv, use the imread function of cv2 module.
+the first parameter is the image path, the second parameter decides if image is to be
+read in black and white format then 0 if in color format then 1.
+"""
+
 img_arr = cv2.imread('image_01.png', 0)
 
+#   print the image in matrix
 print(img_arr)
