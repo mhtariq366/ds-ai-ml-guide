a1f2dc68e0dec530178f079daa6fb2eab74a6ae5
Hassan <mhtariq366@gmail.com>
2025-09-04 22:21:42 +0500

    Update knn.py

--- classification/knn.py
+++ classification/knn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+# import knn classifier from sklearn
 from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv('Datasets/log_reg.csv')
@@ -9,6 +10,8 @@ df = pd.read_csv('Datasets/log_reg.csv')
 
 knn = KNeighborsClassifier()
 
+# fit clasified on data. first parameter is given data and second parameter is label or expected value
 knn.fit(df[['age']], df.gaming)
 
+# once model is trained. predict label. 
 print(knn.predict([[25]]))
